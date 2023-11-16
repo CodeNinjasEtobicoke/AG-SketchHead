f00064d1ca9705e32f4e49ad594c757e0f94ca0f
CodeNinjasEtobicoke <codeninjasetobicoke@outlook.com>
2023-11-16 17:47:15 -0500

    did more

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,14 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    //Platform gameobject
+    [Header("Platform Object")]
+    public GameObject platform;
+    //Defaut position for platform
+    float pos = 0;
     // Start is called before the first frame update
     void Start()
     {
-
+        //Interger i equals 1000
+        for (int i = 0; i < 1000; i++)
+        {
+            //Execute SpawnPlatforms
+            SpawnPlatforms();
+            
+        }
+    }
+    //Spawn platforms function
+     void SpawnPlatforms()
+    {
+        //spawn platforms randomly on the y axis every 2.5
+       Instantiate(platform, new Vector3(Random.value * 10) - 5f, pos, 0.5f), Quaternion.identity);
+        pos
     }
 
     // Update is called once per frame
@@ -16,4 +35,6 @@ public class GameController : MonoBehaviour
     {
 
     }
+
+
 }
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -45,12 +45,29 @@ public class PlayerControls : MonoBehaviour
         }
     }
 
+
+    //Fixedupdate called every fixed frame-rate frame.
+    void FixedUpdate()
     {
-      //Fixedupdate called every fixed frame-rate frame.
-       void FixedUpdate()
-    {
-       //Vector2 which is (x,y) velocity
-       //equals to the velocity of the rigidbody2D
+        //Vector2 which is (x,y) velocity
+        //equals to the velocity of the rigidbody2D
+        Vector2 velocity = rb.velocity;
+        //Velocity of the x axis equals to
+        //the direction movement on the x axis
+        // of the character.
+        velocity.x = movement;
+        //Rigidbody2D = movement;
+        //velocity of the object
+        rb.velocity = velocity;
+
 
     }
+
+    //Collision function
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //velocity with the downspeed
+        rb.velocity = new Vector3(rb.velocity.x, downSpeed, 0);
+    }
+
 }
